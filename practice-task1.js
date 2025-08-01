8fc390bd88d1de533a5c8441b5bb4bdfb809fd58
Naz22135 <mdnazmulhossan360@gmail.com>
2025-08-01 22:53:41 +0600

    conditional-statements added

---
+++ practice-task1.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burger = 600;
+
+if (burger > 500) {
+    console.log('free coke');
+}
+else {
+    console.log('coke price 30tk');
+}
